b25be7f5bd0acf700eca479c4ffe9fc67ed438b0
Artem Sergienko <96276917+Ozsfag@users.noreply.github.com>
2024-12-11 11:20:38 +0900

    init commit

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.3.7-SNAPSHOT"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(23)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+extra["springShellVersion"] = "3.3.3"
+
+dependencies {
+	implementation("org.springframework.shell:spring-shell-starter")
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.shell:spring-shell-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.shell:spring-shell-dependencies:${property("springShellVersion")}")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/com/example/StudentAccountingApplications/StudentAccountingApplicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudentAccountingApplications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAccountingApplicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
